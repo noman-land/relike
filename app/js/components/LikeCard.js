e12df40a38881f13bc0e4d7c0f5ac3c97d8880d3
Noman <noman@noman.land>
2017-07-06 14:57:05 -0400

    Convert LikeCard to functional component

--- app/js/components/LikeCard.js
+++ app/js/components/LikeCard.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ImmutablePropTypes from 'react-immutable-proptypes';
 import PropTypes from 'prop-types';
 
@@ -6,66 +6,60 @@ import Thumb from './Thumb';
 
 import { doesDislike, doesLike } from '../utils/likingUtils';
 
-export default class LikeCard extends Component {
-  static get propTypes() {
-    return {
-      dislikes: PropTypes.number.isRequired,
-      entityId: PropTypes.string.isRequired,
-      likes: PropTypes.number.isRequired,
-      myRating: PropTypes.number.isRequired,
-      onDislikeClick: PropTypes.func.isRequired,
-      onLikeClick: PropTypes.func.isRequired,
-      pendingLikes: ImmutablePropTypes.map.isRequired,
-    };
-  }
+export default function LikeCard({
+  dislikes,
+  entityId,
+  likes,
+  myRating,
+  onDislikeClick,
+  onLikeClick,
+  pendingLikes,
+}) {
+  const likePending = !!(pendingLikes.getIn([entityId, 'like'])
+    || pendingLikes.getIn([entityId, 'unLike']));
 
-  render() {
-    const {
-      dislikes,
-      entityId,
-      likes,
-      myRating,
-      onDislikeClick,
-      onLikeClick,
-      pendingLikes,
-    } = this.props;
+  const dislikePending = !!(pendingLikes.getIn([entityId, 'dislike'])
+    || pendingLikes.getIn([entityId, 'unDislike']));
 
-    const likePending = !!(pendingLikes.getIn([entityId, 'like'])
-      || pendingLikes.getIn([entityId, 'unLike']));
-
-    const dislikePending = !!(pendingLikes.getIn([entityId, 'dislike'])
-      || pendingLikes.getIn([entityId, 'unDislike']));
-
-    return (
-      <div className="flex-column p-0 border-solid border-1 border-radius-2 border-grey-lt m-4-t">
-        <div key={entityId} className="result">
-          <span className="text-center ellipsis text-size-12 p-8-y">
-            {entityId}
-          </span>
-          <div className="flex flex-grow-1 justify-space-between p-4">
-            <Thumb
-              active={doesLike(myRating)}
-              count={likes}
-              direction={'up'}
-              filled={false}
-              onClick={onLikeClick}
-              pending={likePending}
-              textSize={8}
-              thumbSize={14}
-            />
-            <Thumb
-              active={doesDislike(myRating)}
-              count={dislikes}
-              direction={'down'}
-              filled={false}
-              onClick={onDislikeClick}
-              pending={dislikePending}
-              textSize={8}
-              thumbSize={14}
-            />
-          </div>
+  return (
+    <div className="flex-column p-0 border-solid border-1 border-radius-2 border-grey-lt m-4-t">
+      <div key={entityId} className="result">
+        <span className="text-center ellipsis text-size-12 p-8-y">
+          {entityId}
+        </span>
+        <div className="flex flex-grow-1 justify-space-between p-4">
+          <Thumb
+            active={doesLike(myRating)}
+            count={likes}
+            direction={'up'}
+            filled={false}
+            onClick={onLikeClick}
+            pending={likePending}
+            textSize={8}
+            thumbSize={14}
+          />
+          <Thumb
+            active={doesDislike(myRating)}
+            count={dislikes}
+            direction={'down'}
+            filled={false}
+            onClick={onDislikeClick}
+            pending={dislikePending}
+            textSize={8}
+            thumbSize={14}
+          />
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+LikeCard.propTypes = {
+  dislikes: PropTypes.number.isRequired,
+  entityId: PropTypes.string.isRequired,
+  likes: PropTypes.number.isRequired,
+  myRating: PropTypes.number.isRequired,
+  onDislikeClick: PropTypes.func.isRequired,
+  onLikeClick: PropTypes.func.isRequired,
+  pendingLikes: ImmutablePropTypes.map.isRequired,
+};

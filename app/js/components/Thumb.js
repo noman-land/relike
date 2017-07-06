7f159597cff7aa1cf3df05227e7a33eabb035b72
Noman <noman@noman.land>
2017-07-06 15:47:17 -0400

    Convert Thumb to functional component

--- app/js/components/Thumb.js
+++ app/js/components/Thumb.js
@@ -1,62 +1,52 @@
-import classnames from 'classnames';
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
-export default class Thumb extends Component {
-  static get propTypes() {
-    return {
-      active: PropTypes.bool.isRequired,
-      count: PropTypes.number.isRequired,
-      direction: PropTypes.oneOf(['up', 'down']).isRequired,
-      filled: PropTypes.bool.isRequired,
-      onClick: PropTypes.func.isRequired,
-      pending: PropTypes.bool.isRequired,
-      textSize: PropTypes.number.isRequired,
-      thumbSize: PropTypes.number.isRequired,
-    };
-  }
+export default function Thumb({
+  active,
+  count,
+  direction,
+  filled,
+  onClick,
+  pending,
+  textSize,
+  thumbSize,
+}) {
+  const containerClasses = classnames([
+    'align-items-center',
+    'flex',
+    'flex-grow-1',
+    'justify-center',
+    'text-grey',
+    `text-size-${thumbSize}`,
+  ]);
 
-  constructor(props, context) {
-    super(props, context);
-  }
+  const filledClass = filled ? '' : '-o';
 
-  render() {
-    const {
-      active,
-      count,
-      direction,
-      filled,
-      onClick,
-      pending,
-      textSize,
-      thumbSize,
-    } = this.props;
+  const thumbClasses = classnames([
+    'fa',
+    `fa-thumbs${filledClass}-${direction}`,
+    { 'loading-pulsate': pending },
+    { 'text-blue': active },
+  ]);
 
-    const containerClasses = classnames([
-      'align-items-center',
-      'flex',
-      'flex-grow-1',
-      'justify-center',
-      'text-grey',
-      `text-size-${thumbSize}`,
-    ]);
+  return (
+    <div className={containerClasses}>
+      <i aria-hidden="true" className={thumbClasses} onClick={onClick} />
+      <span className={`text-size-${textSize} m-4-l`}>
+        {count}
+      </span>
+    </div>
+  );
+};
 
-    const filledClass = filled ? '' : '-o';
-
-    const thumbClasses = classnames([
-      'fa',
-      `fa-thumbs${filledClass}-${direction}`,
-      { 'loading-pulsate': pending },
-      { 'text-blue': active },
-    ]);
-
-    return (
-      <div className={containerClasses}>
-        <i aria-hidden="true" className={thumbClasses} onClick={onClick} />
-        <span className={`text-size-${textSize} m-4-l`}>
-          {count}
-        </span>
-      </div>
-    );
-  }
+Thumb.propTypes = {
+  active: PropTypes.bool.isRequired,
+  count: PropTypes.number.isRequired,
+  direction: PropTypes.oneOf(['up', 'down']).isRequired,
+  filled: PropTypes.bool.isRequired,
+  onClick: PropTypes.func.isRequired,
+  pending: PropTypes.bool.isRequired,
+  textSize: PropTypes.number.isRequired,
+  thumbSize: PropTypes.number.isRequired,
 };

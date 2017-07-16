7586ab8e4e1de63170a3549cbd9e7e5985910db9
Noman <noman@noman.land>
2017-07-15 23:00:52 -0400

    Give router to Nav

--- app/js/components/Nav.js
+++ app/js/components/Nav.js
@@ -1,19 +1,24 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 
-export default () => (
-  <nav className="nav">
-    <ul className="flex list-style-none p-0 m-0-t">
-      <li>
-        <Link to="/">
-          Home
-        </Link>
-      </li>
-      <li>
-        <Link to="/search">
-          Search
-        </Link>
-      </li>
-    </ul>
-  </nav>
-);
+const Nav = props => {
+  console.log(props);
+  return (
+    <nav className="nav">
+      <ul className="flex list-style-none p-0 m-0-t">
+        <li>
+          <Link to="/">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/search">
+            Search
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default withRouter(Nav);

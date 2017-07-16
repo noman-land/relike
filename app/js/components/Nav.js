7e035f59eed65f76fa1e71e66c745a827aecac3e
Noman <noman@noman.land>
2017-07-15 23:22:23 -0400

    Remove unused props from Nav

--- app/js/components/Nav.js
+++ app/js/components/Nav.js
@@ -1,24 +1,21 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
 
-const Nav = props => {
-  console.log(props);
-  return (
-    <nav className="nav">
-      <ul className="flex list-style-none p-0 m-0-t">
-        <li>
-          <Link to="/">
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link to="/search">
-            Search
-          </Link>
-        </li>
-      </ul>
-    </nav>
-  );
-}
+const Nav = () => (
+  <nav className="nav">
+    <ul className="flex list-style-none p-0 m-0-t">
+      <li>
+        <Link to="/">
+          Home
+        </Link>
+      </li>
+      <li>
+        <Link to="/search">
+          Search
+        </Link>
+      </li>
+    </ul>
+  </nav>
+);
 
 export default withRouter(Nav);

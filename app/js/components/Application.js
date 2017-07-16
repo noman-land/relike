ab0aac82570298f1d2a148d787c4a0a401ba5ebf
Noman <noman@noman.land>
2017-07-15 22:38:30 -0400

    Clean up imports

--- app/js/components/Application.js
+++ app/js/components/Application.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import {
-  BrowserRouter,
-  Route,
-} from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import rootReducer from '../reducers/rootReducer';
 

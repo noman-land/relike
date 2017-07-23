a53f4caffbaa46453895dcce1983868eb7e111de
Noman <noman@noman.land>
2017-07-23 03:15:56 +0200

    Use selectors for all props in SearchPageContainer

--- app/js/containers/SearchPageContainer.js
+++ app/js/containers/SearchPageContainer.js
@@ -3,11 +3,12 @@ import { ReLikeActions } from 'relike-utils';
 
 import SearchPage from '../components/SearchPage';
 
+import { getActiveAccount, getPendingLikes } from '../selectors/coreSelectors';
 import { getSearchResultWithPending } from '../selectors/searchResultSelectors';
 
 const mapStateToProps = state => ({
-  activeAccount: state.activeAccount,
-  pendingLikes: state.pendingLikes,
+  activeAccount: getActiveAccount(state),
+  pendingLikes: getPendingLikes(state),
   searchResult: getSearchResultWithPending(state),
 });
 
--- app/js/selectors/coreSelectors.js
+++ app/js/selectors/coreSelectors.js
@@ -1,3 +1,7 @@
+export function getActiveAccount(state) {
+  return state.activeAccount;
+}
+
 export function getPendingLikes(state) {
   return state.pendingLikes;
 }
